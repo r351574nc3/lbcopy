31354d2685a289d3b6c26d9e2df1b63d15d7fa66
Przybylski 중광 <r351574nc3@gmail.com>
2011-09-11 15:34:20 -0700

    Readding fix that creates sequences and sets the start value

--- src/main/java/liquibase/snapshot/ext/MySQLDatabaseSnapshotGenerator.java
+++ src/main/java/liquibase/snapshot/ext/MySQLDatabaseSnapshotGenerator.java
@@ -19,6 +19,7 @@ import liquibase.statement.SqlStatement;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
+import java.math.BigInteger;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -77,5 +78,42 @@ public class MySQLDatabaseSnapshotGenerator extends liquibase.snapshot.jvm.MySQL
                 rs.close();
             } catch (SQLException ignore) { }
         }
+    }
+
+    protected void readSequences(final DatabaseSnapshot snapshot,                                  
+                                 final String schema, 
+                                 final DatabaseMetaData databaseMetaData) throws DatabaseException {
+        final Database database = snapshot.getDatabase();
+
+        updateListeners("Reading sequences for " + database.toString() + " ...");
+
+        final String convertedSchemaName = database.convertRequestedSchemaToSchema(schema);
+        try {
+            final ResultSet rs = databaseMetaData.getTables(null, convertedSchemaName, null, new String[] { "TABLE" });
+            while (rs.next()) {
+                final String sequenceName = rs.getString("TABLE_NAME");
+                if (isSequence(sequenceName, databaseMetaData)) {
+                    final Sequence seq = new Sequence();
+                    seq.setName(sequenceName.trim());
+                    // seq.setSchema(convertedSchemaName);
+                    snapshot.getSequences().add(seq);
+                }
+            }   
+        }
+        catch (SQLException sqle) {
+            throw new DatabaseException(sqle);
+        }
+    }
+
+    protected boolean isSequence(final String tableName, final DatabaseMetaData databaseMetaData) throws SQLException {
+        final ResultSet rs = databaseMetaData.getColumns(null, null, tableName, null);
+        Integer count = 0;
+        boolean hasId = false;
+        while (rs.next()) {
+            count++;
+            hasId = rs.getString("IS_AUTOINCREMENT").equalsIgnoreCase("yes");
+        }
+
+        return hasId && count == 1;
     }
 }
